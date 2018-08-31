2efcf6687fde853f5cdadd5372079ef06a8291d8
Rajesh Narayana Rao <mailrbn@gmail.com>
2018-08-31 12:57:24 +1000

    31Aug2018

---
+++ MAMPProject/index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Hello World</title>
+</head>
+<body>
+    Hello World!
+    <?php
+        echo "Hello, World of Php!"
+    ?>
+</body>
+</html>
